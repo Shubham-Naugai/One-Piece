044b7494e80fd83dfed729f48c6dca3d0f6ce6c2
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-11 00:14:31 +0530

    Create HomeScreen - add Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
