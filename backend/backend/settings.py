1be8fb3bc2ed4a45613d85491992ddbda6e0562c
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-12 23:48:33 +0530

    Update project settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 
