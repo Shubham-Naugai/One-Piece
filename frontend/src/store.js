021700a012f14a8323e58926ffaef8d46387855c
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-13 01:23:47 +0530

    Update redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,18 @@
-import { configureStore, combineReducer, applyMiddleware, combineReducers } from 'redux'
+import { legacy_createStore, combineReducer, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 
 const initialState = {}
 
 const middleware = [thunk]
 
-const store = configureStore(reducer, initialState,
+const store = legacy_createStore(reducer, initialState,
     composeWithDevTools(applyMiddleware(...middleware)))
 
-// export default store
+export default store
