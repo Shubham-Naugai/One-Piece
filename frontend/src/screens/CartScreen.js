234b09c48d17702b4d741f95be4c897337f326d6
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-13 23:11:18 +0530

    CartScreen AddToCart Functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  Link,
+  useParams,
+  useLocation,
+  useSearchParams,
+  useNavigate,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { Message } from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 function CartScreen() {
-  return (
-    <div>
-        Cart
-    </div>
-  )
+  const { id } = useParams();
+  const search = useLocation().search;
+  
+  const [searchParams] = useSearchParams();
+  const history = useNavigate();
+  
+  const productId = id;
+  const qty = search ? Number(search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart
+  console.log('cartItems: ', cartItems)
+
+  useEffect(() => {
+    if (productId) {
+        dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return <div>Cart</div>;
 }
 
-export default CartScreen
+export default CartScreen;
