d183d072c77f92812a66e036beba858157b8b5cf
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-13 22:02:16 +0530

    Create CartScreen and add to react Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+
 
 
 
@@ -16,6 +18,8 @@ function App() {
             <Routes>
               <Route path='/'  element={<HomeScreen />} exact />
               <Route path='/product/:id' element={<ProductScreen />} />
+              <Route path='/cart/:id?' element={<CartScreen />} />
+
             </Routes>
         </Container>
       </main>
