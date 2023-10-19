0e6df54c1d172f53bb2bb77082bf90e458fe3b6c
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-19 19:35:03 +0530

    Update Urls

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
 
     path('users/profile/', views.getUserProfile, name="user-profile"),
     path('users/', views.getUsers, name="user"),
