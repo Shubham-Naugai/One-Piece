bc0512c190df4e5fdee6ee4c36b3bd7c00a155f4
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-19 19:26:31 +0530

    Authentication and Permission

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
