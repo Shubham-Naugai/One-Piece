42a3fcd1bd44720bfbe1c123f32e8ade1debdb7f
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-20 00:20:23 +0530

    Update user View

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -4,9 +4,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
