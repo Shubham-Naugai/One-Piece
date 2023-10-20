b195cd641ea7a88863deeccfd7d97489f9286035
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-10-20 22:00:17 +0530

    Update Login Screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -11,7 +11,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import FormContainer from "../components/FormContainer";
+// import FormContainer from "../components/FormContainer";
 import { login } from "../actions/userActions";
 
 function LoginScreen() {
